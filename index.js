7d2b5d1f8bfe479c004f7c0a216b35e2c796175d
holgermueller <9jholger@gmail.com>
2024-04-14 14:43:32 -0500

    finished mobile active states

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 "use strict";
+
+const openModal = () => {
+  document.getElementsByClassName("share-section")[0].style.display = "flex";
+  console.log("click");
+};
+
+const closeModal = () => {
+  document.getElementsByClassName("share-section")[0].style.display = "none";
+  console.log("click");
+};
